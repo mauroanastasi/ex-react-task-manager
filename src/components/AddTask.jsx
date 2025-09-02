c50a2cab9a4f6ad85db0015349483b5571a53f97
mauroanastasi <mauroanastasi1@gmail.com>
2025-09-02 12:56:58 +0200

    [UPD] MIL. 5 done. Creazione del Form per Aggiungere un Task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useRef, useMemo } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const AddTask = () => {
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log({ "Nome del Task": nameTask, "Descrizione": refTextArea.current.value, "Status": refSelect.current.value })
+    }
+
+    const [nameTask, setNameTask] = useState("");
+
+    const isUsernameNotValid = useMemo(() => {
+        const symbols = "!@#$%^&*()-_=+[]{}|;:'\"\,.<>?/`~";
+        const valid = [...nameTask].some((n) =>
+            symbols.toLocaleLowerCase().includes(n.toLocaleLowerCase())
+        )
+        return valid || !nameTask.trim()
+    }, [nameTask])
+
+    const refTextArea = useRef()
+    const refSelect = useRef()
+
     return (
         <>
             <header>
@@ -17,6 +36,26 @@ const AddTask = () => {
             <div>
                 <h1>Aggiungi Task</h1>
             </div>
+            <form onSubmit={handleSubmit} action="">
+
+                <label htmlFor="nomeTask">Nome della task: </label>
+                <input type="text" placeholder='scrivi qui' id='nomeTask' value={nameTask} onChange={(e) => setNameTask(e.target.value)} />
+
+                <p style={{ color: isUsernameNotValid ? "red" : "green" }} >{isUsernameNotValid ? "Nome non valido" : "Valido"}</p>
+
+                <label htmlFor="descriz">Descrizione: </label>
+                <textarea ref={refTextArea} id='descriz' ></textarea>
+                <br />
+                <br />
+                <label htmlFor="status">Stato: </label>
+                <select ref={refSelect} id="status">
+                    <option value="To do">To do</option>
+                    <option value="Doing">Doing</option>
+                    <option value="Done">Done</option>
+                </select>
+                <hr />
+                <button type='submit'>Aggiungi Task</button>
+            </form>
         </>
     )
 }
