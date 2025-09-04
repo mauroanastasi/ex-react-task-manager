e516bb2fe5756c740b182ab3881bf1fb00a6c58f
mauroanastasi <mauroanastasi1@gmail.com>
2025-09-04 09:06:32 +0200

    [UPD] MIL. 7 done. Creazione della Pagina Dettaglio Task

--- src/components/TaskDetail.jsx
+++ src/components/TaskDetail.jsx
@@ -1,31 +1,26 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import { useFetchContext } from '../contexts/ContextApi';
 
 const TaskDetail = () => {
 
     const { id } = useParams()
 
-    const apiUrl = import.meta.env.VITE_API_URL
+    const { tasks } = useFetchContext();
 
-    const [detail, setDetail] = useState([])
+    const task = tasks.find((t) => String(t.id) === id);
 
-    const detailsApi = (i) => {
-        axios.get(`${apiUrl}/tasks/${i}`)
-            .then(res => setDetail(res.data))
+    const deleteTask = () => {
+        console.log("elimino task")
     }
-
-    useEffect(() => {
-        detailsApi(id)
-    }, [id])
-
 
     return (
         <>
-            <h2>{detail.title}</h2>
-            <p>{detail.description}</p>
-            <p>{detail.status}</p>
-            <p>{detail.createdAt}</p>
+            <h2>{task.title}</h2>
+            <p>{task.description}</p>
+            <p>{task.status}</p>
+            <p>{task.createdAt}</p>
+            <button onClick={deleteTask} >Elimina Task</button>
         </>
     )
 }
